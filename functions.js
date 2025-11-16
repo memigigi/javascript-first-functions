73e402c0dd1c76f2216025190e9603770185f95c
memigigi <m.sessink@gmail.com>
2025-11-16 19:56:16 +0100

    check console.log

--- functions.js
+++ functions.js
@@ -5,20 +5,28 @@
 function morningGreeter() {
   return "Goedemorgen";
 }
-
 const greeting = morningGreeter();
-
-console.log(greeting);
+console.log(morningGreeter());
 
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
-
+function getSchool() {
+    return "Novi Hogeschool"
+}
+ const school = getSchool();
+console.log(getSchool());
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
+
+function giveCompliment() {
+    return "Lekker bezig met die functies, Jan!";
+}
+ const compliment = giveCompliment();
+ console.log(giveCompliment)
 
 
 
@@ -26,8 +34,19 @@ console.log(greeting);
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
-
+function getGrades(){
+    return[8, 8.5, 6, 7]
+}
+const grades = getGrades();
+console.log(getGrades());
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+function getDetails(){
+    return { firstname: 'Jan', lastname: 'Janssen'};
+}
+const details = getDetails();
+console.log(getDetails());
+
+
